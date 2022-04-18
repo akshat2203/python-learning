4ebe82e437dd71b73be1b47c7a5ce9df56590dff
akshat2203 <akshat.shah2203@gmail.com>
2022-04-18 20:11:08 +0530

    [IMP] classes for learning

---
+++ learn-class.py
@@ -0,0 +1,33 @@
+class Student:
+    """
+        Common base class for all Student
+    """
+    # todo: improve for Inheritance
+    count = 0
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        Student.count += 1
+
+    def student_count(self):
+        print(f"Total student {Student.count}")
+
+    def student_details(self):
+        print(f"Name : {self.name} Salary: {self.marks}")
+
+
+"objects of Student class"
+student_obj = Student("Zara", 98)
+studnet_obj2 = Student("Manni", 5000)
+
+student_obj.student_count()
+student_obj.student_details()
+studnet_obj2.student_count()
+studnet_obj2.student_details()
+
+print(f"Total student {Student.count}")
+
+
+
+
